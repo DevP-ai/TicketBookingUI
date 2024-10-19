2ae637d772151c396bf6aa8800c3cf3fd063e142
Devajit <patar1447@gmail.com>
2024-10-19 11:25:41 +0530

    UI Theme,Shape etc. are setup

--- app/src/main/java/com/technologia/ticketbookingui/ui/theme/Theme.kt
+++ app/src/main/java/com/technologia/ticketbookingui/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Blue,
+    secondary = BlueVariant,
+    tertiary = Yellow
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Blue,
+    secondary = BlueVariant,
+    tertiary = Yellow
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -53,6 +53,7 @@ fun TicketBookingUITheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = content,
+        shapes = Shapes
     )
 }
